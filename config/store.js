83b8741062c7dc5427661cfdca9afa2c4bd38f13
dimon171 <bgluame@gmail.com>
2021-03-26 21:31:39 +0700

    gitignor-add-config

--- config/store.js
+++
@@ -1,6 +0,0 @@
-// config used by store client side only
-module.exports = {
-	// store UI language
-	language: 'ru',
-	ajaxBaseUrl: 'http://localhost:3001/ajax'
-};
---
+++ theme/assets/precache-manifest.390ac395c76ba105a0186f4f4373ebb4.js
@@ -0,0 +1,12 @@
+self.__precacheManifest = [
+  {
+    "url": "/assets/js/theme-a523a92aa16cda03c149.js"
+  },
+  {
+    "url": "/assets/js/app-bb0ecba92ff26365f5b8.js"
+  },
+  {
+    "revision": "a523a92aa16cda03c149",
+    "url": "/assets/css/bundle-00290090e1da93df629f.css"
+  }
+];
