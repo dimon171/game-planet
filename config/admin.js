d91df77af4f9ca07ea14028c2acbc7c42d8da862
dimon171 <71449830+dimon171@users.noreply.github.com>
2021-04-05 21:56:46 +0700

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -2,8 +2,7 @@
 module.exports = {
 	// dashboard UI language
 	language: 'ru',
-	apiBaseUrl: 'http://31.135.36.110/api/v1',
-	apiWebSocketUrl: 'ws://31.135.36.110',
+	apiBaseUrl: 'http://localhost:3001/api/v1',
+	apiWebSocketUrl: 'ws://localhost:3001',
 	developerMode: true
 };
-
